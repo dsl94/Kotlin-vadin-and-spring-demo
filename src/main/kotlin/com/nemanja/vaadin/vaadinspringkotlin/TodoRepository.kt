646a9a99fd40d54082dd91cbf070eba62b2404a9
Nemanja Petrovic <nemanjap@niri.rs>
2017-08-30 08:09:39 +0200

    delete

--- src/main/kotlin/com/nemanja/vaadin/vaadinspringkotlin/TodoRepository.kt
+++ src/main/kotlin/com/nemanja/vaadin/vaadinspringkotlin/TodoRepository.kt
@@ -2,11 +2,13 @@ package com.nemanja.vaadin.vaadinspringkotlin
 
 import com.sun.org.apache.xpath.internal.operations.Bool
 import org.springframework.data.jpa.repository.JpaRepository
+import javax.transaction.Transactional
 
 /**
  * Created by nemanja on 8/29/2017.
  */
 interface TodoRepository: JpaRepository<Todo, Long> {
 
+    @Transactional
     fun deleteByDone(done: Boolean)
 }
